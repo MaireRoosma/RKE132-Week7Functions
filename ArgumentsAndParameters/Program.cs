afe26186699139963969751157d9b01651e77894
Maire Roosma. <roosmamaire@gmail.com>
2024-03-03 16:19:03 +0200

    add Functions code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+Console.WriteLine("Enter a word");
+
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); // userInput  - argumet
+
+
+
+static void PrintAnyWord(string anyString) // anystring - argumet - parameter
+
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
